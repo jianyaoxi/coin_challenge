72d076a325ad3547dced15ea3a5051fc59e3865a
jianyaoxi <2792488354@qq.com>
2026-08-14 20:45:02 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,2 @@
 mkdir coin_env && cd $_ && python -m venv . && source bin/activate && cd .. 
-pip install -r coinenv_requirements.txt
+pip install retrying flask attrs gymnasium colorama accelerate transformers==4.43.1 Pillow opencv-python dotenv qwen-vl-utils huggingface_hub google-genai openai
